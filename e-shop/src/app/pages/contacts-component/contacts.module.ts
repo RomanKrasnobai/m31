576fc059b97921ee7bb19a3ff7af788685076f6c
RomanKrasnobai <krasnobay@gmail.com>
2019-06-28 14:01:08 +0300

    fix styles add links

--- e-shop/src/app/pages/contacts-component/contacts.module.ts
+++ e-shop/src/app/pages/contacts-component/contacts.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent } from './contacts-component.component';
 import { CommonModule } from '@angular/common';
+import {SharedComponentsModule} from '../../shared-components/shared-components.module';
 
 @NgModule({
     declarations: [
         ContactsComponent,
     ],
-    imports: [
-        CommonModule,
-        ContactsRoutingModule,
-    ],
+  imports: [
+    CommonModule,
+    ContactsRoutingModule,
+    SharedComponentsModule,
+  ],
     providers: []
   })
   export class ContactsModule { }
--- e-shop/src/app/pages/payment-delivery-component/payment-delivery.module.ts
+++ e-shop/src/app/pages/payment-delivery-component/payment-delivery.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { PaymentDeliveryComponent } from './payment-delivery-component.component';
 import { CommonModule } from '@angular/common';
 import { PaymentDeliveryRoutingModule } from './payment-delivery-routing.module';
+import {SharedComponentsModule} from '../../shared-components/shared-components.module';
 
 @NgModule({
     declarations: [
         PaymentDeliveryComponent,
     ],
-    imports: [
-        CommonModule,
-        PaymentDeliveryRoutingModule,
-    ],
+  imports: [
+    CommonModule,
+    PaymentDeliveryRoutingModule,
+    SharedComponentsModule,
+  ],
     providers: []
   })
   export class PaymentDeliveryModule { }
