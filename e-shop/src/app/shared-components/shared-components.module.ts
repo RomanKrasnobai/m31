e5dbc0373db806f42e71fa7004b37016619ea727
RomanKrasnobai <krasnobay@gmail.com>
2019-06-28 23:50:37 +0300

    add payment-delivery info

--- e-shop/src/app/shared-components/shared-components.module.ts
+++ e-shop/src/app/shared-components/shared-components.module.ts
@@ -3,20 +3,23 @@ import { FooterComponent } from './footer-component/footer-component.component';
 import { HeaderComponent } from './header-component/header-component.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 @NgModule({
     declarations: [
         FooterComponent,
         HeaderComponent,
+        LeftMenuComponent,
     ],
     imports: [
         CommonModule,
         RouterModule,
     ],
-    exports: [
-        FooterComponent,
-        HeaderComponent
-    ],
+  exports: [
+    FooterComponent,
+    HeaderComponent,
+    LeftMenuComponent
+  ],
     providers: []
   })
   export class SharedComponentsModule { }
