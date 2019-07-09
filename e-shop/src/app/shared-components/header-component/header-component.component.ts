e7a50e8fe6425a6f8f8c0608951036ac1c6799a3
Oleksandr Pavlovskyi <oleksandr.pavlovskyi@certent.com>
2019-07-09 12:25:53 +0300

    e-shop header resources url

--- e-shop/src/app/shared-components/header-component/header-component.component.ts
+++ e-shop/src/app/shared-components/header-component/header-component.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  get location(): Location {
+    return document.location;
+  }
+
+  constructor() {}
 
   ngOnInit() {
   }
