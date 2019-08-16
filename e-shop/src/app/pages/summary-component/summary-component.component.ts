ebccc824449eaff0c7119d1b8d323aa25db28e59
Roman.Krasnobai <Roman.Krasnobai@innovecs.com>
2019-08-16 14:26:19 +0300

    some fix

--- e-shop/src/app/pages/summary-component/summary-component.component.ts
+++ e-shop/src/app/pages/summary-component/summary-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {OrderBasket} from "../../models/OrderBasket";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-summary-component',
@@ -16,12 +16,21 @@ export class SummaryComponent implements OnInit {
 
   ngOnInit() {
     this.order = JSON.parse(localStorage.getItem('productsOrder'));
+    this.formValidation();
+
     console.log('sum',this.order);
   }
 
-  // formValidation() {
-  //   this.orderForm = this.fb.group()
-  // }
+  formValidation() {
+    this.orderForm = this.fb.group({
+      city: ['', Validators.required],
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      npPost: [false],
+      npCourier: [false],
+      ukrPost: [false],
+    })
+  }
 
   getSummaryBasket() {
     let sum = 0;
