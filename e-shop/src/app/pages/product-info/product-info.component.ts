468d1d56fb6748113418550be1f75649502a0194
Roman.Krasnobai <Roman.Krasnobai@innovecs.com>
2019-10-18 13:51:47 +0300

    add page product info

--- e-shop/src/app/pages/product-info/product-info.component.ts
+++ e-shop/src/app/pages/product-info/product-info.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {map, switchMap} from 'rxjs/operators';
 import {ItemsService} from '../../services/items.service';
+import {Item} from '../../models/item';
 
 @Component({
   selector: 'app-product-info-component',
@@ -10,13 +11,17 @@ import {ItemsService} from '../../services/items.service';
 })
 export class ProductInfoComponent implements OnInit {
 
-  product: any;
-  selectedId: any;
+  product: Item;
   constructor(
     private activatedRoute: ActivatedRoute,
     private itemsService: ItemsService) { }
 
   ngOnInit() {
-
+    this.itemsService.getById(this.activatedRoute.snapshot.paramMap.get('id')).subscribe(
+      item => {
+          this.product = item;
+          console.log(this.product);
+        }
+    );
   }
 }
